e732dcffd48c506632fe9e21158eb88d6961fd50
Gonzalo Carmenado <gonzalocarmenado@gmail.com>
2025-05-03 12:26:02 +0200

    Add: create interceptor library

---
+++ projects/general-http-interceptor/src/lib/general-http-interceptor.service.ts
@@ -0,0 +1,39 @@
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { catchError, finalize, Observable, throwError } from 'rxjs';
+
+
+
+export const GeneralHttpInterceptorService: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+  debugger
+  req = req.clone({
+    headers: req.headers.set('Authorization', 'Tu token aqui')
+  });
+
+
+  if (!req.headers.has('Content-Type')) {
+    req = req.clone({
+      headers: req.headers.set('Content-Type', 'application/json; charset=utf-8')
+    });
+  }    
+
+  req = req.clone({
+    headers: req.headers.set('Accept', 'application/json')
+  });
+
+  req = req.clone({
+    headers: req.headers.set('Platform', 'web')
+  });
+
+  return next(req).pipe(
+    catchError((err: HttpErrorResponse) => {
+      if (err.status === 401) {
+        window.location.href = `URL de prohibido o login`;
+      }
+       
+      return throwError(err);
+    }),
+    finalize(() => {
+    })
+  );
+  
+};
---
+++ projects/general-http-interceptor/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of general-http-interceptor
+ */
+
+export * from './lib/general-http-interceptor.service';
